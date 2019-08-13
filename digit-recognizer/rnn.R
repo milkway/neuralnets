630c22c29009fba177684c6377beeb7ca41e9e1b
milkway <leite@de.ufpe.br>
2019-08-13 13:22:25 -0300

    using test image

--- digit-recognizer/rnn.R
+++ digit-recognizer/rnn.R
@@ -12,13 +12,16 @@ img_cols <- 28
 n_classes <- 10
 
 # Loading Data
-kaggle_test <- read_csv("~/Downloads/digit-recognizer/kaggle_test.csv")
-kaggle_train <- read_csv("~/Downloads/digit-recognizer/kaggle_train.csv")
+#kaggle_test <- read_csv("~/Downloads/digit-recognizer/kaggle_test.csv")
+#kaggle_train <- read_csv("~/Downloads/digit-recognizer/kaggle_train.csv")
 
+test <- read_rds("test.rds")
+label <- read_rds("label_test.rds")
+train <- read_rds("train.rds")
 
 # Visualizing a random digit
-
-kaggle_test %>% 
+train %>% 
+  select(-label, -Flag) %>% 
   sample_n(1) %>%   
   unlist() %>%
   matrix(nrow = 28, byrow = TRUE) %>% 
@@ -35,6 +38,11 @@ Label <- function(digits, base, nrows = 28, ncols = 28){
   {
     cat("\nRemoving label column...")
     base <- base %>%  select(-label)
+  }
+  if ("Flag" %in% colnames(base)) 
+  {
+    cat("\nRemoving Flag column...")
+    base <- base %>%  select(-Flag)
   }
   # Check graphical parameters 
   val <- par(no.readonly=TRUE)
@@ -54,12 +62,14 @@ Label <- function(digits, base, nrows = 28, ncols = 28){
 }
 
 # Call Function
-Label(1:10, kaggle_train)
+Label(101:109, train)
 
 library(keras)
+set.seed(123)
+
 #### Organize data to keras 
-train <- kaggle_train %>% 
-  mutate(Flag = sample(1:0, nrow(kaggle_train), replace = TRUE, prob = c(.10, .90)))  
+# train <- kaggle_train %>% 
+#   mutate(Flag = sample(1:0, nrow(kaggle_train), replace = TRUE, prob = c(.10, .90)))  
 
 x_train <- train %>% 
   filter(Flag == 0) %>% 
@@ -117,3 +127,49 @@ rnn_model %>% evaluate(x_test, y_test)
 
 # Predict
 rnn_model %>% predict_classes(x_test)
+
+Input <- wgts[[1]]
+Input_bias <- wgts[[2]]
+Layer <- wgts[[3]]
+Layer_bias <- wgts[[4]]
+Output <- wgts[[5]]
+Output_bias <- wgts[[6]]
+
+Y = x_test[1,] %*% Input + t(Input_bias)
+Y_relu = Y * (Y > 0) # Activation Function
+Z = Y_relu %*% Layer + t(Layer_bias)
+Z_relu = Z * (Z > 0) # Activation Function
+W = Z_relu %*% Output + t(Output_bias)
+f_exp = exp(W)
+W_softmax = f_exp/sum(f_exp)# softmax output
+
+cat(round(W_softmax))
+
+## Test image
+library(imager)
+
+digit <- load.image("digit.jpeg")
+digit28 <- digit %>%  resize(size_x = 28, size_y = 28, interpolation_type = 1L)
+
+digit28 <- rowMeans(digit28, dims = 2)
+
+digit28 %>% apply(1, rev) %>% 
+  t() %>% 
+  image(col = grey.colors(256), axes = FALSE)
+
+
+digit_test <- as.vector(digit28)
+
+Y = (1 - digit_test) %*% Input + t(Input_bias)
+Y_relu = Y * (Y > 0) # Activation Function
+Z = Y_relu %*% Layer + t(Layer_bias)
+Z_relu = Z * (Z > 0) # Activation Function
+W = Z_relu %*% Output + t(Output_bias)
+f_exp = exp(W)
+W_softmax = f_exp/sum(f_exp)# softmax output
+
+cat(round(W_softmax))
+
+cat(round(W_softmax) %*% (0:9))
+
+hist(digit_test)
